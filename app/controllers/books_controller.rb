f88312155c3bdfece42b28e16ce3728bf658a598
kyu-3hiya4 <177545609+kyu-3hiya4>
2024-08-26 13:32:07 +0000

    3-12 create作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,12 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books/show'
+  end
+
   def index
   end
 
@@ -11,5 +17,11 @@ class BooksController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
