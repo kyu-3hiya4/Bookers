0bfe75da3efd706c313208ca99a203440239e5c5
kyu-3hiya4 <177545609+kyu-3hiya4>
2024-08-27 09:08:58 +0000

    [add]update機能

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # get 'books/index'
   # get 'books/show'
   # get 'books/edit'
-
+ patch 'books/:id' => 'books#update', as: 'update_book'
   get '/top' => 'homes#top'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
