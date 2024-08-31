2c4356edbd205448cfe98a875c27c77d9db1e5bc
kyu-3hiya4 <177545609+kyu-3hiya4>
2024-08-31 14:19:13 +0000

    [Add]バリデーション実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,9 +2,14 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    flash[:notice] = "Book was successfully created."
-    redirect_to book_path(book.id)
+    if book.save
+     flash[:notice] = "Book was successfully created."
+     redirect_to book_path(book.id)
+    else
+      @books = Book.all
+      @book = Book.new
+      render :index
+    end
   end
 
   def index
@@ -22,9 +27,13 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = "Book was successfully updated."
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+     flash[:notice] = "Book was successfully updated."
+     redirect_to book_path(book.id)
+    else
+      @book = Book.find(params[:id])
+      render :edit
+    end
   end
 
   def destroy
